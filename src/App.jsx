5d6e676769fba3cc88e40bb9028d5c51e9e139a3
Evan Hymanson <evanhymanson@Evans-MacBook-Pro.local>
2024-12-01 14:18:11 -0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar>hi</Sidebar>
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
